eeda9bf579f62f7c9f1c59d22f1372976729aa68
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-06-01 17:59:30 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const SHARE_ICON = document.querySelector(".share-icon");
 const SHARE_BOX = document.querySelector(".share-popup");
+const BODY = document.querySelector("body");
 
 SHARE_ICON.addEventListener("click", () => {
     SHARE_BOX.classList.toggle("display");
